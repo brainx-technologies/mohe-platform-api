b3da72d9d356fcd46c06337ad00df9897453cbdc
Christian Thieme <c.thieme@me.com>
2023-04-11 16:39:26 +0200

    deprectaed fields for backwards mobile compatibility

--- mohe_api/accounts/serializers.py
+++ mohe_api/accounts/serializers.py
@@ -4,6 +4,23 @@ from mohe.client.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    admin_url = serializers.SerializerMethodField()
+    dwh_url = serializers.SerializerMethodField()
+    team_name = serializers.SerializerMethodField()
+    token = serializers.SerializerMethodField()
+
+    def get_admin_url(self, obj):
+        return 'deprecated'
+
+    def get_team_name(self, obj):
+        return 'deprecated'
+
+    def get_dwh_url(self, obj):
+        return 'deprecated'
+
+    def get_token(self, obj):
+        return 'deprecated'
+
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'email')
+        fields = ('id', 'first_name', 'last_name', 'email', 'team_name', 'admin_url', 'dwh_url', 'token')
