6219dc37d9d02f88aea932ea1d307e92ca81eeac
Christian Thieme <c.thieme@me.com>
2023-06-26 16:09:38 +0200

    patient: comment and photo added

--- mohe_api/patient/serializers.py
+++ mohe_api/patient/serializers.py
@@ -6,4 +6,4 @@ from mohe.patient.models import Patient
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
-        fields = ('id', 'first_name', 'last_name', 'birthday', 'sex')
+        fields = ('id', 'first_name', 'last_name', 'birthday', 'sex', 'comment', 'photo')
